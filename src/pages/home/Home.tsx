73cdd57794c8b4d956ee719830c03d454099f23e
M.Ali Yücesoylu <myucesoylu@gmail.com>
2022-12-22 19:30:01 +0300

    footer created

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 import Featured from "../../components/featured/Featured";
 import FeaturedRooms from "../../components/featuredRooms/FeaturedRooms";
+import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
 import MailList from "../../components/mailList/MailList";
 import Navbar from "../../components/navbar/Navbar";
@@ -19,6 +20,7 @@ const Home = () => {
         <FeaturedRooms />
 
         <MailList />
+        <Footer />
       </div>
     </div>
   );
